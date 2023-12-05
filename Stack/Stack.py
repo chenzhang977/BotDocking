28c0abd2aec4b5f7cdc37e3e5cef8e0f26f14649
chenzhang <chenzhang@sandboxol.com>
2023-12-05 10:38:41 +0800

    sync

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -5,9 +5,7 @@ import schedule
 
 import Util.DB.DB as DB
 import Util.BF2042 as BF2042
-import Config.Config as Config
 import Util.Message.MessageManager as MessageManager
-from Util.Message.Message import MessageType
 
 def init_stack():
     thread = threading.Thread(target = start_task)
@@ -27,20 +25,7 @@ def create_stack():
     # 消息队列
     schedule.every(1).seconds.do(MessageManager.send_sync_message)
     # 更新战绩
-    schedule.every(1).hours.do(update_bf_info)
+    schedule.every(1).hours.do(BF2042.update_all_info)
     # 战绩播报
-    schedule.every(8).hours.do(record_broadcast)
+    schedule.every(8).hours.do(BF2042.record_broadcast)
     #schedule.every(5).seconds.do(record_broadcast)
-
-def update_bf_info():
-    name_list = BF2042.get_all_name()
-    for name in name_list:
-        print(BF2042.get_info_by_api(name[0]))
-
-def record_broadcast():
-    name_list = BF2042.get_all_name()
-    for name in name_list:
-        msg_type = MessageType.TG
-        group_id = msg_type == MessageType.TG and Config.tg.tg_self_group or Config.qq.qq_group_id
-        message = MessageManager.create_sync_message(group_id = group_id, msg = BF2042.get_record(name[0]),type = msg_type)
-        MessageManager.add_message(message)
--- Util/BF2042.py
+++ Util/BF2042.py
@@ -2,9 +2,16 @@ import json
 import traceback
 import requests
 
+import Util.DB.DB as DB
+import Config.Config as Config
+import Util.Message.MessageManager as MessageManager
+
+from Util.Message.Message import MessageType
 from datetime import datetime, timedelta
 
-import Util.DB.DB as DB
+def bind_id(id: int, name: str):
+    cmd = f"INSERT OR REPLACE INTO user (uid, bf_name) VALUES ('{id}', '{name}')"
+    return  DB.execute(cmd)
 
 def get_info_by_api(name: str):
     response = requests.get('https://api.gametools.network/bf2042/stats?name=' + name)
@@ -14,13 +21,13 @@ def get_info_by_api(name: str):
         deaths = json_obj['deaths']
         damage = json_obj['damage']
         if kill > 0:
-            update_info(name, kill, deaths, damage)
+            update_db(name, kill, deaths, damage)
 
         return {'name':name, 'kill': kill, 'deaths': deaths, 'damage': damage}
     
     return "查询异常, 请查看log"
 
-def update_info(name: str, kill: int, deaths: int, damage: int):
+def update_db(name: str, kill: int, deaths: int, damage: int):
     updateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     cmd = f"INSERT INTO BF (updateTime, name, kill, deaths, damage) VALUES ('{updateTime}','{name}', '{kill}', '{deaths}', '{damage}')"
     try:
@@ -77,10 +84,6 @@ def get_record(name: str, hours: int = 24):
 
     return  f"name: {name}\nkill: {kill}({max_kill})\ndeaths: {deaths}({max_deaths})\ndamage: {damage}({max_damage})\nkd: {kd}({max_kd})"
 
-def bind_id(id: int, name: str):
-    cmd = f"INSERT OR REPLACE INTO user (uid, bf_name) VALUES ('{id}', '{name}')"
-    return  DB.execute(cmd)
-
 def get_name(id: int):
     cmd = f"SELECT DISTINCT bf_name FROM user WHERE uid='{id}'"
     ret = DB.execute(cmd)
@@ -94,5 +97,20 @@ def get_all_name():
     cmd = "SELECT DISTINCT name FROM BF"
     ret = DB.execute(cmd)
     return ret
+
+def update_all_info():
+    print('开始更新所有玩家信息')
+    name_list = get_all_name()
+    for name in name_list:
+        print(get_info_by_api(name[0]))
+    print('玩家信息更新结束')
+
+def record_broadcast():
+    name_list = get_all_name()
+    for name in name_list:
+        msg_type = MessageType.TG
+        group_id = msg_type == MessageType.TG and Config.tg.tg_self_group or Config.qq.qq_group_id
+        message = MessageManager.create_sync_message(group_id = group_id, msg = get_record(name[0]), type = msg_type)
+        MessageManager.add_message(message)
 
 #DB.execute("DELETE FROM BF WHERE name = 'aaa';")
