6e9e97cf986a36d316e78b486e6b88bab1874022
chenzhang <chenzhang@sandboxol.com>
2023-12-06 20:48:10 +0800

    tg代理

--- Util/TG/TGBotClient.py
+++ Util/TG/TGBotClient.py
@@ -14,7 +14,7 @@ def get_time_stamp(time)->int:
 def init(name : str):
     global app
     app = Client(name)
-    #app.set_proxy(hostname = "proxy.example.com", port = 8080, username = "proxy_username", password = "proxy_password")
+    app.set_proxy(hostname = "1270.0.0.1", port = 7890)
 
 def run():
     global app
